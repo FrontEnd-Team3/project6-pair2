baf802822f1df73cb4a7149a7f8dc65fcbb50d7e
ohwphil <ohwphil@gmail.com>
2023-06-17 01:30:48 +0900

    feat: rtk 작업함

--- issue-viewer/src/pages/main/index.js
+++ issue-viewer/src/pages/main/index.js
@@ -1,59 +1,29 @@
+import { Octokit } from "@octokit/rest";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Pagination from "./componetns/pagination";
+import { useDispatch, useSelector } from "react-redux";
+import { setApi } from "../../reducer/detail";
 
 const MainPage = () => {
-  const { Octokit } = require("@octokit/rest");
-
   const octokit = new Octokit({
-    auth: "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",
+    // auth: "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",
     // auth: "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",
   });
 
-  const [api, setApi] = useState([]);
-  const [limit, setLimit] = useState(10);
+  // const [api, setApi] = useState([]);
+  const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
+  const [sort, setSort] = useState("created");
   const pageCount = parseInt(200 / limit);
+  const api = useSelector((state) => state.api.apis);
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
   useEffect(() => {
-    const fetchData = async () => {
-      const requests = Array.from({ length: pageCount }).map(async (_, i) => {
-        const response = await octokit.request(
-          "GET /repos/{owner}/{repo}/issues",
-          {
-            owner: "angular",
-            repo: "angular-cli",
-            per_page: limit,
-            page: i + 1,
-            sort: "created",
-          }
-        );
-        return response.data;
-      });
-      const data = await Promise.all(requests);
-      setApi(data);
-    };
-    fetchData();
-  }, [limit]);
+    dispatch(setApi({ limit, sort }));
+  }, [limit, sort]);
 
-  // return (
-  //   <div>
-  //     {api.length > 0 &&
-  //       api[currPage - 1] &&
-  //       api[currPage - 1].map((v) => (
-  //         <div onClick={() => navigate(`/${v.id}`)}>
-  //           <div>number : {v.number}</div>
-  //           <div>id : {v.id}</div>
-  //           <div>title : {v.title}</div>
-  //           <div>user : {v.user.login}</div>
-  //           <div>comments : {v.comments}</div>
-  //           <div>create_date : {v.created_at}</div>
-  //           <div>update_date : {v.updated_at}</div>
-  //         </div>
-  //       ))}
-  //   </div>
-  // );
   return (
     <div>
       <div>

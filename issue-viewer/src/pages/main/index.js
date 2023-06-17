a303cf722c812c96aa2914bab30428eeefd4177a
sueddd <phs8743@naver.com>
2023-06-17 20:18:55 +0900

    css완성

--- issue-viewer/src/pages/main/index.js
+++ issue-viewer/src/pages/main/index.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Pagination from "./componetns/pagination";
 import { useDispatch, useSelector } from "react-redux";
 import { setApi } from "../../reducer/detail";
+import styled from "styled-components";
 
 const MainPage = () => {
   const octokit = new Octokit({
@@ -26,10 +27,10 @@ const MainPage = () => {
   }, [limit, sort]);
 
   return (
-    <div>
+    <High_Container>
       <div>
-        <label>
-          페이지 당 표시할 게시물 수:&nbsp;
+        <Wrapper_page>
+          Select the number of pages
           <select
             type="number"
             value={limit}
@@ -39,9 +40,9 @@ const MainPage = () => {
             <option value="50">50</option>
             <option value="100">100</option>
           </select>
-        </label>
-        <label>
-          filter:
+        </Wrapper_page>
+        <Wrapper_filter>
+          Select a Page Filter
           <select
             value={sort}
             onChange={({ target: { value } }) => setSort(value)}
@@ -50,24 +51,107 @@ const MainPage = () => {
             <option value="updated">updated</option>
             <option value="comments">comments</option>
           </select>
-        </label>
+        </Wrapper_filter>
       </div>
-      {api.length &&
-        api[page - 1] &&
-        api[page - 1].map((v) => (
-          <div onClick={() => navigate(`/${v.id}`)}>
-            <div>number : {v.number}</div>
-            <div>id : {v.id}</div>
-            <div>title : {v.title}</div>
-            <div>user : {v.user.login}</div>
-            <div>comments : {v.comments}</div>
-            <div>create_date : {v.created_at}</div>
-            <div>update_date : {v.updated_at}</div>
-          </div>
-        ))}
+      <Container>
+        {api.length &&
+          api[page - 1] &&
+          api[page - 1].map((v) => (
+            <Wrapper_post onClick={() => navigate(`/${v.id}`)}>
+              <div>{v.title}</div>
+              <div>
+                <div>#{v.id}</div>
+                <div>comments({v.comments})</div>
+              </div>
+              <div>{v.body}</div>
+              <div>
+                create {v.created_at} , update {v.updated_at}
+              </div>
+            </Wrapper_post>
+          ))}
+      </Container>
       {console.log(api.length)}
       <Pagination numPages={pageCount} page={page} setPage={setPage} />
-    </div>
+    </High_Container>
   );
 };
 export default MainPage;
+const High_Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  top: 30px;
+`;
+
+const Wrapper_post = styled.div`
+  margin: 0 auto;
+  border: 1px solid darkgray;
+  box-shadow: 1px 3px 1px darkgray;
+  text-align: center;
+  line-height: 2;
+  width: 1000px;
+  margin-top: 30px;
+  border-radius: 30px;
+  font-weight: 600;
+  color: #666666;
+  div:nth-child(2) {
+    display: flex;
+    justify-content: space-around;
+    font-weight: 600;
+    color: darkgray;
+  }
+  div:nth-child(3) {
+    width: 800px;
+    white-space: nowrap;
+    margin: 0 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  div:nth-child(4) {
+    color: darkgray;
+    position: relative;
+    right: 20px;
+    text-align: right;
+    font-weight: 600;
+  }
+`;
+const Wrapper_page = styled.label`
+  padding: 10px 10px;
+  position: relative;
+  top: 30px;
+  left: 350px;
+  font-size: 18px;
+  color: gray;
+  font-weight: 500;
+  select {
+    margin-left: 10px;
+    padding: 2px;
+    font-size: 15px;
+    position: relative;
+    bottom: 1px;
+    background-color: gray;
+    border-radius: 4px;
+    color: white;
+  }
+`;
+
+const Wrapper_filter = styled.label`
+  padding: 10px 10px;
+  position: relative;
+  top: 30px;
+  left: 350px;
+  font-size: 18px;
+  color: gray;
+  font-weight: 500;
+  select {
+    margin-left: 10px;
+    padding: 2px;
+    font-size: 15px;
+    position: relative;
+    bottom: 1px;
+    background-color: gray;
+    border-radius: 4px;
+    color: white;
+  }
+`;
